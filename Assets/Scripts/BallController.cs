061df8df08f67b063c2b2b406fcd144c4a76b2ac
Darius Ashley <44679539+Raptorizer@users.noreply.github.com>
2026-07-09 14:24:24 -0500

    Ball and Camera components

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BallController : MonoBehaviour
+{
+    public GameObject directionalArrow;
+    [SerializeField] private int maxDistance;
+    [SerializeField] private float shootPower;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
